0e8e03e7f994e72f4ee6baefb91943d2feead89c
jiehuan-jason <jiehuan233@outlook.com>
2024-10-13 08:15:30 +0800

    Fix search bugs

--- src/top/jiehuan/kilikili/URLget.java
+++ src/top/jiehuan/kilikili/URLget.java
@@ -9,7 +9,7 @@ import javax.microedition.io.Connector;
 import javax.microedition.io.HttpConnection;
 
 public class URLget {
-	static int maxBytes = 10240;
+	static int maxBytes = 40960;
 	public static String IP_ADDRESS="localhost";
 	public static String SEARCH_URL="http://"+IP_ADDRESS+":3000/search?keyword=";
 	public static String USER_INFO_URL="http://"+IP_ADDRESS+":3000/user?mid=";
@@ -205,7 +205,7 @@ public class URLget {
     
 	}
 	 public static String BackWeb(String url){
-			HttpConnection connection = null;
+		 HttpConnection connection = null;
 	        DataInputStream dis =null;
 	        
 	        InputStream inputStream = null;
@@ -213,33 +213,44 @@ public class URLget {
 	        int num=0;
 
 	        try {
-	            // 打开连接
-	        	
+	            // 打开连接 设置请求方式和请求类型
 	            connection = (HttpConnection) Connector.open(url);
 	            connection.setRequestMethod(HttpConnection.GET);
-	            connection.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
-	           
+	            connection.setRequestProperty("Content-Type", "application/json; charset=UTF-8"); 
+	            
+	            // 连接
 	            num = connection.getResponseCode();
 	            
+	            // 输出返回的网页代码
 	            System.out.println("get now");
-	            System.out.println(connection.getResponseCode());
+	            System.out.println(num);
 	            
 	            if(num==200){
-
 	            	dis=connection.openDataInputStream();
 	            	int length = (int)connection.getLength();
 	            	String str=null;
 	            	
 	            	if(length!=-1){
-	            		System.out.println("length!=-1");
-	            		byte[] incomingData = new byte[length];
-	            		dis.read(incomingData);
-	            		System.out.println("read over");
-	            		System.out.println("length: " + length);
-	            		System.gc();
-	            		str=new String(incomingData,"UTF-8");
-	            		System.out.println("Test OK");
-	            		System.out.println(str);
+	            		byte[] buffer = new byte[2048]; // 1KB 缓冲区
+	                    int bytesRead = 0;
+	                    int totalBytesRead = 0;
+	                    StringBuffer jsonPart = new StringBuffer();
+
+	                    // 逐块读取数据
+	                    while ((bytesRead = dis.read(buffer)) != -1) {
+	                        if (totalBytesRead + bytesRead > maxBytes) {
+	                            bytesRead = maxBytes - totalBytesRead; // 只读取剩余的字节
+	                            System.out.println("reading...");
+	                        }
+	                        jsonPart.append(new String(buffer, 0, bytesRead,"UTF-8"));
+	                        totalBytesRead += bytesRead;
+	                        
+	                        if (totalBytesRead >= maxBytes) {
+	                            break; // 达到最大字节数，停止读取
+	                        }
+	                    }
+	                    System.out.println(jsonPart.toString()); // 输出获取到的内容
+	                    str=jsonPart.toString();
 	            	}else{
 	            		System.out.println("length==-1");
 	            		ByteArrayOutputStream bs=new ByteArrayOutputStream();
@@ -250,34 +261,57 @@ public class URLget {
 	            		str=new String(bs.toByteArray(),"UTF-8");
 	            		bs.close();
 	            	}
-	            	System.out.println(str);
+	            	// 关闭HTTP连接
+	            	try {
+						connection.close();
+					} catch (IOException e1) {
+						return "error"+e1.getMessage();
+					}
+	            	// 关闭DataInputStream
+		        	if(dis!=null){
+		        		try{
+		        			dis.close();
+		        		}catch(Exception e){
+		        			return "error"+e.getMessage();
+		        		}
+		        	}
+		        	// 打印获取到的bilibili返回代码，并检查是否返回错误代码
+		        	String code = FindString.findValueInt(str,"code");
+		        	System.out.println("code:"+code);
+		        	if(code.equals("-400")){
+		        		return "No This BVID.";
+		        	}
 	            	return str;
 	            }
 	        }catch(Exception e){
 	        	e.printStackTrace();
-	        	return "error";
+	        	return "error"+e.getMessage();
 	        }finally{
+	        	// 关闭HTTP连接
 	        	try {
 					connection.close();
 				} catch (IOException e1) {
-					return "connection close error";
+					return "error"+e1.getMessage();
 				}
+	        	// 关闭inputStream连接
 	        	if(inputStream!=null){
 	        		try{
 	        			inputStream.close();
 	        		}catch(Exception e){
-	        			return "inputStream close error";
-	        		}
-	        	}if(dis!=null){
+	        			return "error"+e.getMessage();
+	        		}	
+	        	}
+	        	// 关闭DataInputStream连接
+	        	if(dis!=null){
 	        		try{
 	        			dis.close();
 	        		}catch(Exception e){
-	        			return "DataInputStream close error";
+	        			return "error"+e.getMessage();
 	        		}
 	        	}
 	        	
 	        }
-	        return "back"+num;
+	        return "back "+num;
 	            
 	    
 		}
